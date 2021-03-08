45a954eb79a4feb63ac9499d2b70239078ae5a56
Tom <tom.marx@hotmail.fr>
2021-03-08 23:18:35 +0100

    Twitter tests and implementation

---
+++ src/twitter.py
@@ -0,0 +1,28 @@
+if (__name__ == "__main__"):
+	exit(1)
+
+import json
+import tweepy
+
+api = None
+
+def connect():
+	global api
+	creds = json.loads(open("./creds.json").read())
+	auth = tweepy.OAuthHandler(
+		creds['twitter']['apiKey'], 
+		creds['twitter']['secret']
+	)
+	auth.set_access_token(
+		creds['twitter']['accessToken'], 
+		creds['twitter']['accessTokenSecret']
+	)
+	api = tweepy.API(auth)
+	api.verify_credentials()
+
+def tweet(text, media=None):
+	if (media):
+		media = api.media_upload(media)
+		return api.update_status(status=text, media_ids=[media.media_id])
+	else:
+		return api.update_status(status=text)
